43380dbd7d3465795bb12c8d5fbc53d3e609e4c0
abhishek <abhishek12mishra@gmail.com>
2020-10-04 13:45:12 +0530

    auth connect to signin screen

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -67,7 +67,9 @@ router.post('/signin',(req,res)=>{
             {
                 //res.json({message:"logged in successfully"})
                 const token=jwt.sign({_id:savedUser._id},JWT_SECRET);
-                res.json({token})
+                const {_id,name,email}=savedUser;
+                
+                res.json({token,user:{_id,name,email}})
             }
             else
             return res.status(422).json({error:"invalid email or password"})
