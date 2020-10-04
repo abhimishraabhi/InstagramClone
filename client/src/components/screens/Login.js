cd14dd2441cb4289d495b5e488b0dd7b7160a3fc
abhishek <abhishek12mishra@gmail.com>
2020-10-04 13:41:06 +0530

    now signin screen able to interact with server and after sign in it redirect to home screen of user and throws token

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,8 +1,45 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React,{useState} from 'react'
+import {Link,useHistory} from 'react-router-dom'
+import M from 'materialize-css'
 
 
 const Login=()=>{
+
+
+    const History=useHistory()
+    
+    const [password,setPassword]=useState("")
+    const [email,setEmail]=useState("")
+    const PostData=()=>{
+        fetch("/signin",{
+        method:"post",    
+        headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                
+                password,
+                email
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+
+            console.log(data)
+
+            if(data.error)
+            {
+                M.toast({html: data.error,classes:"#b71c1c red darken-4"})
+            }
+            else
+            {
+                M.toast({html:"signed in successfully",classes:"#2e7d32 green darken-3"})
+                History.push('./')
+            }
+            
+        }).catch(err=>{
+            console.log(err);
+        })
+    }
 
     
 
@@ -14,13 +51,21 @@ const Login=()=>{
           <input
           type='text'
           placeholder="email"
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
           />
+
+
            <input
           type='text'
           placeholder="password"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+
           />
 
-           <button className="btn waves-effect waves-light #64b5f6 blue darken-1" type="submit" name="action">
+           <button className="btn waves-effect waves-light #64b5f6 blue darken-1" type="submit" name="action"
+           onClick={()=>PostData()} >
                Login
            
             </button>
