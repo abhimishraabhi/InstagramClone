de8d0e96a97a456f4634f2201bceab422784b88b
abhishek <abhishek12mishra@gmail.com>
2020-10-05 10:53:42 +0530

    App.js added and its style

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Navbar from './components/Navbar'
+import './App.css'
+import {BrowserRouter,Route} from 'react-router-dom' 
+
+import './App.css';
+import Home from './components/screens/Home'
+import Profile from './components/screens/Profile'
+import Login from './components/screens/Login'
+import Signup from './components/screens/Signup'
+import CreatePost from './components/screens/CreatePost'
+
+function App() {
+  return ( 
+
+    <BrowserRouter>
+    <Navbar/>
+    <Route exact path='/'>
+      <Home/>
+    </Route>
+    <Route path='/signin'>
+      <Login/>
+    </Route>
+    <Route path='/signup'>
+      <Signup/>
+    </Route>
+    <Route path='/profile'>
+    <Profile/>
+    </Route>
+    <Route path='/createpost'>
+    <CreatePost/>
+    </Route>
+    </BrowserRouter>
+
+
+
+    )
+  
+}
+
+export default App;
