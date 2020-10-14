6b299adbce7bf87aeaf4d77db8f5d0d8d8b5273d
abhishek <abhishek12mishra@gmail.com>
2020-10-14 11:49:06 +0530

    require login to see posts and posts become dynamic

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose');
 const requireLogin=require("../middleware/requireLogin")
 const Post=mongoose.model('Post')
 
-router.get('/allpost',(req,res)=>{
+router.get('/allpost',requireLogin,(req,res)=>{
     Post.find()
     .populate('postedBy',"_id name")
     .then(posts=>{
@@ -17,9 +17,9 @@ router.get('/allpost',(req,res)=>{
 })
 
 router.post('/createpost',requireLogin,(req,res)=>{
-    const {title,body}=req.body;
+    const {title,body,pic}=req.body;
 
-    if(!title||!body)
+    if(!title||!body||!pic)
     {
         res.status(422).json({error:"pleade add all the fields"});
     }
@@ -31,6 +31,7 @@ router.post('/createpost',requireLogin,(req,res)=>{
     const post=new Post({
         title,
         body,
+        photo:pic,
         postedBy:req.user
     })
     post.save().then(result=>{
